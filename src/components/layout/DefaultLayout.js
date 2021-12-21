1f7d3970ce900d09aedab223e3bed29b1b742d87
Gaurav <gauravsing1195@gmail.com>
2021-12-21 14:02:32 +0530

    breadcrumb added

--- src/components/layout/DefaultLayout.js
+++ src/components/layout/DefaultLayout.js
@@ -7,7 +7,7 @@ export const DefaultLayout = ({children}) => {
             <div className="default-layout">
 
 
-            <header className='header'>
+            <header className='header mb-2'>
             <Header/>
              </header>
             <main className='main' >
